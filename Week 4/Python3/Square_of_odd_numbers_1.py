c97be0ed14cc1d7e7c65382fbf623b7ed7a6e59e
Gagan Raj <gaganraj2002@gmail.com>
2022-09-02 15:03:19 +0530

    Issue with problem statement,Test cases not cleared

--- Week 4/Python3/Square_of_odd_numbers_1.py
+++ Week 4/Python3/Square_of_odd_numbers_1.py
@@ -1,3 +1,4 @@
+# Issue with the problem statement, 1 out of 2 test cases passed
 #li = [int(x) for x in input().split(",")]
 #out = [x*x for x in li if x % 2 != 0]
 #st = ""
@@ -6,6 +7,7 @@
 #st = st[:-1]
 # print(st)
 #
+
 li = [int(x) for x in input().split(",")]
 st = ''
 for i in range(len(li)+1):
