6667dc050052394d81ddc0b1c47618484c60c567
Gagan Raj <gaganraj2002@gmail.com>
2022-09-04 17:00:51 +0530

    Week 4 Sqaure of odd numbers 1 .re-tried

--- Week 4/Python3/Square_of_odd_numbers_1.py
+++ Week 4/Python3/Square_of_odd_numbers_1.py
@@ -1,17 +1,23 @@
 # Issue with the problem statement, 1 out of 2 test cases passed
-#li = [int(x) for x in input().split(",")]
-#out = [x*x for x in li if x % 2 != 0]
-#st = ""
-# for i in out:
-#    st += str(i)+","
-#st = st[:-1]
-# print(st)
-#
+def checkConsecutive(l):
+    dif_ = sorted(l)[1]-sorted(l)[0]
+    return sorted(l) == list(range(min(l), max(l)+1, dif_))
+
 
 li = [int(x) for x in input().split(",")]
-st = ''
-for i in range(len(li)+1):
-    if i % 2 != 0:
-        st += str(i*i)+","
-st = st[:-1]
-print(st)
+if checkConsecutive(li):
+    out = [x*x for x in li if x % 2 != 0]
+    st = ""
+    for i in out:
+        st += str(i)+","
+    st = st[:-1]
+    print(st)
+
+
+#li = [int(x) for x in input().split(",")]
+#st = ''
+# for i in range(len(li)+1):
+#    if i % 2 != 0:
+#        st += str(i*i)+","
+#st = st[:-1]
+# print(st)
