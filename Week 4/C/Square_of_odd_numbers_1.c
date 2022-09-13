73add7cd065c5d6794d97926bbe4b524df7328bb
Gagan Raj <gaganraj2002@gmail.com>
2022-09-13 18:54:44 +0530

    week 4 Square of odd numbers passed test case

--- Week 4/C/Square_of_odd_numbers_1.c
+++ Week 4/C/Square_of_odd_numbers_1.c
@@ -1,28 +1,27 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
- 
-int main() 
-{ 
-  int array[1024]; 
-  int length=0; 
-  char buffer[1024]; 
-  char *aux; 
-  fgets(buffer,1023,stdin); 
-  aux=strtok(buffer, ","); 
-  while(aux) 
-  { 
-  array[length]=atoi(aux); 
-  length++; 
-  aux=strtok(NULL, ","); 
-  } 
-  for(int i=0;i<length;i++) 
-  { 
-    if (array[i]%2!=0){
-        if (i!=length-1)
-   printf("%d,",array[i]*array[i]);
-        else
-            printf("%d",array[i]*array[i]);
-    } 
-  } 
-} 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+int a[10000],i,s;
+    for(i=0;i<10000;i++)
+    { scanf("%d,",&a[i]);
+     if(a[i]==NULL)
+     {
+         break;
+     }
+     else if(i==0&&(a[i]%2)!=0)
+     {
+         s=a[i]*a[i];
+         printf("%d",s);
+     }
+       else if(a[i]%2!=0)
+       {
+         s=a[i]*a[i];
+           printf(",%d",s);
+       }
+    }
+    return 0;
+}
