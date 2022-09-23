129e8bed249cf731555a5588f2eaf5de15b42b2c
Gagan Raj <gaganraj2002@gmail.com>
2022-09-23 16:06:43 +0530

    Week 5 Python Working

--- Week 5/Python3/Subarray_with_given_sum_11.py
+++ Week 5/Python3/Subarray_with_given_sum_11.py
@@ -1,21 +1,11 @@
-import collections
-
-
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        ans = sums = 0
-        cnt = collections.Counter()
-        for num in nums:
-            cnt[sums] += 1
-            sums += num
-            ans += cnt[sums - k]
-        return ans
-
-
-x = Solution()
-print(x.subarraySum([1, 2, 3, 7, 5], 12))
+n, req_sum = (int(x) for x in input().split())
+lst = [int(x) for x in input().split()]
+for i in range(n):
+    c = 0
+    for j in range(i, n):
+        c += lst[j]
+        if c == req_sum:
+            print(i+1, j+1)
+            break
+    if (c == req_sum):
+        break
